34ead645f269d6409a6d380b3e4f3dafe61b8311
Marcelo Moreira <96429088+Marcelo-devweb@users.noreply.github.com>
2023-04-09 21:34:02 -0300

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "adicao",
+        hint: "O processo de somar números"
+    },
+    {
+        word: "reuniao",
+        hint: "Evento em que as pessoas se reúnem"
+    },
+    {
+        word: "numero",
+        hint: "Símbolo matemático usado para contar"
+    },
+    {
+        word: "troca",
+        hint: "O ato de negociar"
+    },
+    {
+        word: "tela",
+        hint: "Pedaço de tecido para pintura a óleo"
+    },
+    {
+        word: "jardim",
+        hint: "Espaço para plantar flor e planta"
+    },
+    {
+        word: "posicao",
+        hint: "Localização de alguém ou algo"
+    },
+    {
+        word: "pena",
+        hint: "Cabelo como cobertura externa de pássaro"
+    },
+    {
+        word: "conforto",
+        hint: "Uma agradável sensação de relaxamento"
+    },
+    {
+        word: "lingua",
+        hint: "O órgão muscular da boca"
+    },
+    {
+        word: "expansao",
+        hint: "O processo de aumentar ou crescer"
+    },
+    {
+        word: "pais",
+        hint: "Uma região politicamente identificada"
+    },
+    {
+        word: "grupo",
+        hint: "Vários objetos ou pessoas"
+    },
+    {
+        word: "gosto",
+        hint: "Habilidade da língua para detectar o sabor"
+    },
+    {
+        word: "loja",
+        hint: "Grande loja onde as mercadorias são negociadas"
+    },
+    {
+        word: "campo",
+        hint: "Área de terreno para atividades agropecuárias"
+    },
+    {
+        word: "amigo",
+        hint: "Pessoa que não seja membro da família"
+    },
+    {
+        word: "bolso",
+        hint: "Uma bolsa para carregar pequenos itens"
+    },
+    {
+        word: "agulha",
+        hint: "Um alfinete de metal fino e afiado"
+    },
+    {
+        word: "especialista",
+        hint: "Pessoa com amplo conhecimento"
+    },
+    {
+        word: "declaracao",
+        hint: "Uma declaração de algo"
+    },
+    {
+        word: "segundo",
+        hint: "Um sexagésimo de minuto"
+    },
+    {
+        word: "biblioteca",
+        hint: "Local contendo coleção de livros"
+    },
+]
